e72bfcb2338ee567c3d4f8d99aa109c0775e1c61
donplay-pro <56683477+donplay-pro@users.noreply.github.com>
2020-10-16 19:51:54 +0530

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int lcm(int a, int b)
+{
+	int i=1;
+	while(i%a != 0 || i%b != 0)
+	i++;
+	return i;
+}
+int main()
+{
+	int a,b,l;
+	scanf("%d%d",&a,&b);
+	l= lcm(a,b);
+	printf("lcm = %d",l);
+	return 0;
+}
