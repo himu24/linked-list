71dd0aa7b1048d09cc8e321f2d4f4cb5a1a12749
donplay-pro <56683477+donplay-pro@users.noreply.github.com>
2020-10-16 19:02:28 +0530

    Add files via upload

---
+++ deleteheap.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+int arr[100],n;
+
+void swap(int a,int b)
+{
+	int k;
+	k=arr[a];
+	arr[a]=arr[b];
+	arr[b]=k;
+}
+void printfun()
+{
+	int i;
+	for(i=1;i<=n;i++)
+	printf("%d ",arr[i]);
+	printf("\n");
+}
+void deleteheap()
+{
+	int i,c,k;
+	for(i=n;i>=1;i--)
+	{
+		c=1;
+		k=arr[1];
+		arr[1]=arr[i];
+		arr[i]=0;
+		while(2*c<i)
+		{
+			if(arr[c*2]<arr[c*2+1])
+			{
+				if(arr[c*2+1]>arr[c])
+				swap(c*2+1,c);
+				else
+				break;
+				c=c*2+1;
+			}
+			else
+			{ if(arr[c*2]>arr[c])
+				swap(c*2,c);
+				else
+				break;
+				c=c*2;
+		    }
+		}
+		arr[i]=k;
+		printfun();
+	}
+}
+int main()
+{
+	int i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	scanf("%d",&arr[i]);
+	deleteheap();
+	
+	return 0;
+}
